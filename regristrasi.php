fa48e029a1ce579ed606cf4c83af93e1267513b6
sandi putra perdana <43519591+Sandipp1@users.noreply.github.com>
2018-10-02 16:53:02 +0700

    Update regristrasi.php

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,29 @@
-<!DOCTYPE html>
 <html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
-
-
-
-</body>
+<center>
+	<table>
+	<form action="prosesregris.php" method="POST">
+		<tr>
+		<td>NIM</td> 
+		<td>:</td>
+		<td><input type="text" name="nim" maxlength="10" minlength="10"><br></td>
+	</tr>
+	<tr>
+		<td>Nama</td> 
+		<td>:</td> 
+		<td><input type="text" name="nama"><br></td>
+	</tr>
+	<tr>
+		<td>Email</td> 
+		<td>:</td> 
+		<td><input type="email" name="email" value="@gmail.com"><br></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td><input type="submit" name="submit" value="submit"></td>
+	</tr>
+</form>
+</table>	
+</center>
 </html>
+
+  
