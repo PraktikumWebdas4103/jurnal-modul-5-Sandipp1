cb654bf11421eabe4a81782852d3167d48b01a20
sandi putra perdana <43519591+Sandipp1@users.noreply.github.com>
2018-10-02 16:52:39 +0700

    Update prosesregris.php

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,14 @@
 <?php
-
-
+if (isset($_POST['submit'])){
+$nim= $_POST['nim'];
+$nama = $_POST['nama'];
+$email = ($_POST['email']);
+include "koneksi.php";
+$sql ="INSERT INTO regis (nim,nama,email) values ('$nim','$nama','$email')";
+if (mysqli_query($con,$sql)){
+	echo "Registrasi Telah Berhasil";
+}else{
+	echo "Gagal Regis";
+}
+}
 ?>
